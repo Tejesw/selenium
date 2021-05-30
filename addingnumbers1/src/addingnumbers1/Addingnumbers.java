4862f65130cf34416ffe66e7aaf673fda240a212
Tejesw <tejeswar718@gmail.com>
2021-05-30 18:51:39 +0530

    selenium2

---
+++ addingnumbers1/src/addingnumbers1/Addingnumbers.java
@@ -0,0 +1,15 @@
+package addingnumbers1;
+
+public class Addingnumbers {
+
+	public static void main(String[] args) {
+				int num1=10;
+				int num2=20;
+				int result = num1+num2;
+				System.out.println("the result of the following is:"+result);
+
+			}
+
+		}
+
+
