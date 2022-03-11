a8a5bd2ce56033b9c744a629128746ca526ebb37
Tejesw <tejeswar718@gmail.com>
2022-03-11 17:29:34 +0530

    hjhjhuuuu

---
+++ Multiplication/src/multiplication.java
@@ -0,0 +1,12 @@
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		int a=10,b=25,c;
+		c=a*b;
+		System.out.println("Product of two numbers is");
+		System.out.println(c);
+
+	}
+
+}
